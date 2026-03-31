0e6c94bd6e30c903dc64f5d0dacdf6abc24c5011
CopperShavings <adama4169@masseyhigh.school.nz>
2026-03-31 15:06:10 +1300

    instructions

---
+++ C_01_BF_instructions.py
@@ -0,0 +1,27 @@
+def instructions():
+    """Prints instructions"""
+
+    print("""
+ *** Instructions ****
+
+- To begin, choose the number of questions you would
+ like to answer (or press <enter> for infinite mode).
+
+
+- Then you need to pick whether you would like to use default 
+parameters (1, 12) or pick your own!
+
+If you decide to choose your own, select a low number and 
+a high number (high must be more than low.)
+
+
+- You need to try guess get as many answers correct as possible!
+You'll be answering addition, subtraction, multiplication, and 
+division questions
+
+- Select <xxx> to end the game.
+
+Good luck!
+
+
+    """)
