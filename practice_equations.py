f7939a90a28032a65acbb163ca0f90dffc86e039
CopperShavings <adama4169@masseyhigh.school.nz>
2026-04-21 12:28:16 +1200

    random addition question generator - practice

--- practice_equations.py
+++ practice_equations.py
@@ -1,7 +1,10 @@
 import random
 
-x = random.randint(num_1, num_2)
-y = random.randint(num_1, num_2)
+num1 = random.randint
+num2 = random.randint
+
+x = random.randint(num1, num2)
+y = random.randint(num1, num2)
 
 answer3 = x+y
 print (f"{x}+{y} = {answer3}")
